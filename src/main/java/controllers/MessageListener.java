f3b9d5083764e0a2f874192324f4168f9e7b48b8
rpfister102 <rpfister@hsr.ch>
2018-10-02 09:48:43 +0200

    friend rejection

--- src/main/java/controllers/MessageListener.java
+++ src/main/java/controllers/MessageListener.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 public interface MessageListener {
     void onFriendRequest(Person requester);
     void onFriendConfirm(Person sender);
+    void onFriendRejection(Person sender);
     void onFriendRemoval(Person sender);
     void onGroupInvitation(String groupKey) throws IOException, ClassNotFoundException;
     void onGroupLeave(String groupKey) throws IOException, ClassNotFoundException;
--- src/main/java/dtos/FriendRejectionMessage.java
+++ src/main/java/dtos/FriendRejectionMessage.java
@@ -1,4 +1,20 @@
 package dtos;
 
-public class FriendRejectionMessage {
+public class FriendRejectionMessage implements Message {
+
+    private String sender;
+
+    public FriendRejectionMessage(String sender) {
+        this.sender = sender;
+    }
+
+    @Override
+    public MessageType getType() {
+        return MessageType.FRIEND_REJECTION;
+    }
+
+    @Override
+    public String getSenderUsername() {
+        return sender;
+    }
 }
