4e6c61f0b92d686df50a1d7cdd703baf185d9fc5
Keerthikan@Asus <kk.devpro11@gmail.com>
2018-10-05 20:52:52 +0200

    create chat detail ui

--- src/main/java/views/PrivateChatDetailView.java
+++ src/main/java/views/PrivateChatDetailView.java
@@ -2,6 +2,7 @@ package views;
 
 import controllers.ChatController;
 import models.Person;
+import views.fragments.MessageSendFragment;
 
 import javax.swing.*;
 import java.awt.*;
@@ -9,26 +10,38 @@ import java.awt.*;
 public class PrivateChatDetailView extends JPanel {
     private ChatController controller;
     private Person person;
+    private DefaultListModel<String> messageListModel;
 
     public PrivateChatDetailView(ChatController controller, Person person) {
         this.controller = controller;
         this.person = person;
 
         createView();
+        updateData();
     }
 
-    private void createView(){
-        setBackground(new Color(242,242, 242));
+    private void updateData() {
+        for(int i = 0; i < 10; i++){
+            messageListModel.addElement(String.format("Hello " + i));
+        }
+    }
+
+    private void createView() {
+        setBackground(new Color(27, 27, 27));
+        setLayout(new BorderLayout(0, 0));
         setPreferredSize(new Dimension(MainPanel.DETAIL_WIDTH, MainPanel.TOTAL_HEIGHT));
         createHeader();
+        createChatDetails();
+        createMessageSend();
     }
 
     private void createHeader() {
         Box titlePanel = Box.createHorizontalBox();
         titlePanel.setBackground(Color.WHITE);
+        titlePanel.setOpaque(true);
         titlePanel.setAlignmentX(Component.LEFT_ALIGNMENT);
-        titlePanel.setPreferredSize(new Dimension(MainPanel.DETAIL_WIDTH, 40));
-        titlePanel.setBorder(BorderFactory.createMatteBorder(0, 0, 1, 0,
+        titlePanel.setPreferredSize(new Dimension(MainPanel.DETAIL_WIDTH, 39));
+        titlePanel.setBorder(BorderFactory.createMatteBorder(1, 1, 1, 0,
                 new Color(74, 126, 187)));
 
         JLabel title = new JLabel(person.getName());
@@ -42,6 +55,24 @@ public class PrivateChatDetailView extends JPanel {
         titlePanel.add(onlineStatus, BorderLayout.EAST);
 
         add(titlePanel, BorderLayout.NORTH);
+    }
+
+    private void createChatDetails(){
+        JTabbedPane tabbedPane = new JTabbedPane();
+
+        messageListModel = new DefaultListModel<>();
+        JList list = new JList(messageListModel);
+        JScrollPane listScroll = new JScrollPane(list);
+        listScroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
+
+        tabbedPane.addTab("Messages", null, listScroll);
+        tabbedPane.addTab("Notary Messages", null, new JLabel("Hello 2"));
+
+        add(tabbedPane, BorderLayout.CENTER);
+    }
+
+    private void createMessageSend() {
+        add(new MessageSendFragment(), BorderLayout.SOUTH);
     }
 
 }
---
+++ src/main/java/views/fragments/MessageSendFragment.java
@@ -0,0 +1,40 @@
+package views.fragments;
+
+import views.MainPanel;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MessageSendFragment extends JPanel {
+    private JTextArea messageBox;
+    private JButton btnSend;
+
+    public MessageSendFragment(){
+        createView();
+    }
+
+    private void createView() {
+        setOpaque(false);
+        Box panel = Box.createHorizontalBox();
+//        panel.setOpaque(false);
+        panel.setPreferredSize(new Dimension(MainPanel.DETAIL_WIDTH, 50));
+        panel.add(Box.createHorizontalStrut(5));
+
+        messageBox = new JTextArea("test");
+        messageBox.setRows(2);
+        messageBox.setPreferredSize(new Dimension(MainPanel.DETAIL_WIDTH - 150, 50));
+        messageBox.setEditable(true);
+        panel.add(messageBox);
+
+        btnSend = new JButton("Send");
+        btnSend.setPreferredSize(new Dimension(80, 50));
+        btnSend.setBackground(new Color(44, 93, 152));
+        btnSend.setForeground(Color.WHITE);
+        btnSend.setFont(new Font("Arial", Font.PLAIN, 18));
+        panel.add(Box.createHorizontalStrut(5));
+        panel.add(btnSend);
+        panel.add(Box.createHorizontalStrut(5));
+
+        add(panel, BorderLayout.CENTER);
+    }
+}
