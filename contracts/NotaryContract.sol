f719031fe5c51fe513c9c9228ad3cb8c2d30db17
Keerthikan@Asus <kk.devpro11@gmail.com>
2018-10-31 08:27:01 +0100

    update contract with abi and bin

--- contracts/NotaryContract.sol
+++ contracts/NotaryContract.sol
@@ -2,6 +2,8 @@ pragma solidity ^0.4.25;
 
 contract NotaryContract {
 
+    address owner;
+
     struct State {
         Status status;
         bool isSet;
@@ -13,33 +15,38 @@ contract NotaryContract {
         REJECTED
     }
 
-    mapping(bytes32 => State) states;
-    mapping(bytes32 => address) recipients;
+    event StateChanged(
+        address accepter,
+        bytes32 msgHash,
+        Status state
+    );
 
-    address owner;
+    mapping(bytes32 => State) states;
+    mapping(address => bytes32) recipients;
 
     constructor () public { owner = msg.sender; }
 
     function kill() public { if (msg.sender == owner) selfdestruct(owner); }
 
-    function addMessage(bytes32 hash, address recipient) public {
+    function addMessage(bytes32 hash) public {
         require(states[hash].isSet == false);
         states[hash] = State(Status.PENDING, true);
-        recipients[hash] = recipient;
     }
 
     function acceptMessage(bytes32 hash) public {
         require(states[hash].isSet == true);
         require(states[hash].status == Status.PENDING);
-        require(recipients[hash] == msg.sender);
+        recipients[msg.sender] = hash;
         states[hash].status = Status.ACCEPTED;
+        emit StateChanged(msg.sender, hash, Status.ACCEPTED);
     }
 
     function rejectMessage(bytes32 hash) public {
         require(states[hash].isSet == true);
         require(states[hash].status == Status.PENDING);
-        require(recipients[hash] == msg.sender);
+        recipients[msg.sender] = hash;
         states[hash].status = Status.REJECTED;
+        emit StateChanged(msg.sender, hash, Status.REJECTED);
     }
 
     function getMessageState(bytes32 hash) public view returns (Status) {
